b50d15fe06fcea7d57251c5fe24bd8437929b5a5
Fernando Panduro <92656447+fernandopanduro@users.noreply.github.com>
2024-01-22 20:27:10 -0700

    ✨ feat: Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,38 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Redesing Room",
-  description: "Rediseña tu cuarto con Inteligencia Artificial",
+  title: "Room Redesign: 🚀 Transforma tu Espacio con la Potencia de la IA",
+  description:
+    "Descubre Room Redesign, la innovadora herramienta de diseño de interiores impulsada por inteligencia artificial. 🖼️ Sube una imagen de tu habitación y explora más de 20 estilos de diseño para recibir visualizaciones fotorrealistas en segundos. ¡Da vida a tus ideas de renovación con facilidad y creatividad! ✨",
+  keywords:
+    "Room Redesign, diseño de interiores, inteligencia artificial, rediseño de habitaciones, estilos de diseño, visualización fotorrealista, creatividad en decoración, renovación de hogar",
+  applicationName: "Room Redesign",
+  authors: [{ name: "Fernando Panduro", url: "https://ferpanduro.com" }],
+  creator: "Fernando Panduro",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://room-redesing.vercel.app/",
+    title: "Room Redesign: 🚀 Transforma tu Espacio con la Potencia de la IA",
+    description:
+      "Descubre Room Redesign, la innovadora herramienta de diseño de interiores con inteligencia artificial.",
+    siteName: "Room Redesign",
+    images: [
+      {
+        url: "https://room-redesing.vercel.app/",
+        alt: "Room Redesign",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    // site: "@tucuenta", // Reemplaza con tu nombre de usuario de Twitter
+    // creator: "@tucuenta", // Reemplaza con tu nombre de usuario de Twitter
+    title: "Room Redesign: 🚀 Transforma tu Espacio con la Potencia de la IA",
+    description:
+      "Descubre Room Redesign, la innovadora herramienta de diseño de interiores con inteligencia artificial.",
+    images: "https://room-redesing.vercel.app/og-image.jpg", // Reemplaza con la URL de tu imagen para Twitter
+  },
 };
 
 export default function RootLayout({
